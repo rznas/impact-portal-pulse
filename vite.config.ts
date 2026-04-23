2523e3e3a1f0c55fb16e7b123c4e85ef9f377af1
reza nasri <reza.nasri@digikala.com>
2026-04-23 13:12:07 +0330

    updagte

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
     vite: {
       server: {
-        allowedHosts: ['*']
+        allowedHosts: ['hajhossein.geimm.ir', 'icheck.com']
       },
     },
   },
